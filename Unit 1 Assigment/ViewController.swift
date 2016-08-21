17baa9632035b06d1f2905e5722644d24afc6787
Richard Kelly II <richard.kellyii@capitalone.com>
2016-08-21 17:40:41 -0400

    Initial Commit

---
+++ Unit 1 Assigment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Unit 1 Assigment
+//
+//  Created by Kelly II, Richard W. on 8/21/16.
+//  Copyright © 2016 Kelly II, Richard W. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
